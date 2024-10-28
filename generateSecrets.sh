6ac0c08d3849e8303e7fb3c222e669f4fb5acd55
david.pangerl <david.pangerl@flare.network>
2024-10-28 08:35:05 +0100

    ficed generate secrets to work in bash

--- generateSecrets.sh
+++ generateSecrets.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v ($pwd)/secrets.json.template:/usr/src/app/secrets.json.template fasset-bots yarn key-gen generateSecrets --agent $1 --merge secrets.json.template --other $2 > secrets.new.json
+docker run --rm -v $PWD/secrets.json.template:/usr/src/app/secrets.json.template fasset-bots yarn key-gen generateSecrets --agent $1 --merge secrets.json.template --other $2 > secrets.new.json
