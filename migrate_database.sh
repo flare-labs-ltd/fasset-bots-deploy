0ef7d37df75d9a829c64ec453e3d238fb244f6b4
kuco23 <nseverkar@gmail.com>
2025-01-15 14:51:34 +0100

    fix: docker-compose -> docker compose

--- migrate_database.sh
+++ migrate_database.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-echo $(docker-compose --profile cli run -T --entrypoint sh agent-bot \
+echo $(docker compose --profile cli run -T --entrypoint sh agent-bot \
     -c "yarn agent-bot listAgents -f FXRP -c ./config.json -s ./secrets.json")
